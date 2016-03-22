2b4664cb8455010d1d329c3af46777c2d7812cfd
Tyler Seabrook <tseabrook@ingrooves.com>
2016-03-21 21:45:22 -0700

    Switch to component and fix itemDetails bug

--- js/directives/itemDetails.js
+++ js/directives/itemDetails.js
@@ -1,9 +1,15 @@
-app.directive('itemDetails', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            info: '='
-        },
-        templateUrl: 'js/directives/itemDetails.html'
-    };
+app.component('itemDetails', {
+    bindings: {
+        info: '=' // things we want pass to the controller from our html tag.
+    },
+    controllerAs: 'ctrl', // '$ctrl' is the default name but I think it looks ugly :)
+    controller: function($scope) {
+        var self = this;
+
+        this.descriptionIsString = function() {
+            // we can see self.info is a thing here because it's in the bindings
+            return typeof(self.info.description) === 'string';
+        }
+    },
+    templateUrl: 'js/directives/itemDetails.html'
 });
