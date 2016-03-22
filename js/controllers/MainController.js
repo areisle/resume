ee73f88d570ba9ba73f229353002b7c020ba42f7
areisle <areisle@gmail.com>
2016-03-21 20:30:36 -0700

    added essential square brackets

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,21 +2,21 @@ app.controller('MainController', ['$scope', function ($scope) {
     $scope.sections = [
         {
             title: "Related Skills",
-            item:
+            items:[
                 {
                     location: "Technical",
                     description: ["description of duties point 1","description of duties point 2"] 
-                }
+                }]
         },
         {
             title: "Work Experience",
-            item:
+            items:[
                 {
                     location: "company",
                     title: "job title",
                     date: "date of employment 2",
                     description: "description of duties 2"
-                }
+                }]
         }
     ]
 }]);
